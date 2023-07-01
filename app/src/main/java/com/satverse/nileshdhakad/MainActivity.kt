47db67eb1db77c79bc195ed49e423e00e632af55
Satyam Sharma <satyamsharma1725@gmail.com>
2023-07-01 16:51:21 +0530

    initial commit

---
+++ app/src/main/java/com/satverse/nileshdhakad/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.satverse.nileshdhakad
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private val url = "https://nileshdhakad.co/" // Replace with your website URL
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.databaseEnabled = true
+        webSettings.allowFileAccess = true
+        webSettings.loadsImagesAutomatically = true
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webView.loadUrl(url)
+        webView.setWebViewClient(object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                view.loadUrl(url)
+                return false
+            }
+        })
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
